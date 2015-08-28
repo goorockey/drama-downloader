bdb77c8196dffdc292daee3175e836167efd7364
goorockey <kelvingu616@gmail.com>
2015-08-28 12:07:22 +0800

    Add taskbar and history functions

--- drama_download.py
+++ drama_download.py
@@ -4,18 +4,26 @@ import sys
 import logging
 import json
 import re
+import pickle
+import os
 import argparse
 import ConfigParser
 import requests
 import sched
 import time
-from datetime import datetime
+from datetime import datetime, date
 from lxml import html
 from baidupcsapi import PCS
 from cloudsight import recognize_img
 
 
 _CONF_FILE = 'config.ini'
+_LOG_FILE = 'history.log'
+
+_pcs = None
+_history = {}
+
+logging.basicConfig()
 logger = logging.getLogger('drama_downloader')
 logger.setLevel(logging.INFO)
 
@@ -30,7 +38,7 @@ def _parse_conf(conf_file):
         sys.exit(-1)
 
 
-def _baidupan_login(conf):
+def _get_pcs(conf):
     def _captcha_callback(img):
         logger.info('Try to recognize captcha...')
         result = recognize_img(img)
@@ -43,6 +51,10 @@ def _baidupan_login(conf):
 
         return ''
 
+    global _pcs
+    if _pcs:
+        return _pcs
+
     try:
         username = conf.get('baidupan', 'username')
         password = conf.get('baidupan', 'password')
@@ -50,23 +62,27 @@ def _baidupan_login(conf):
             logger.error('No username or password found.')
             return
 
-        pcs = PCS(username,
+        _pcs = PCS(username,
                   password,
                   captcha_callback=_captcha_callback)
 
         logger.info('Baidupan login successfully.')
 
-        return pcs
+        return _pcs
     except:
         import traceback
         traceback.print_exc()
         logger.error('Failed to login in baidupan.')
-
-
-def download_drama(conf):
-    pcs = _baidupan_login(conf)
-    if not pcs:
         sys.exit(-1)
+
+
+def download_drama(args):
+    conf = _parse_conf(args.config)
+    _LOG_FILE = args.log
+
+    _load_history()
+
+    today = date.today()
 
     for key, value in conf.items('drama'):
         url, day, rule = map(lambda x: x.strip(), value.split(';'))
@@ -75,7 +91,11 @@ def download_drama(conf):
             continue
 
         day = int(day)
-        if day <= 0 or day + 1 != datetime.today().weekday():
+        if day <= 0 or day - 1 != datetime.today().weekday():
+            continue
+
+        last_download = _get_history(key)
+        if last_download is not None and last_download >= today:
             continue
 
         try:
@@ -90,7 +110,8 @@ def download_drama(conf):
                 logger.error('No resource found. (key=%s)', key)
                 continue
 
-            # TODO: save history to avoid duplicate
+
+            pcs = _get_pcs(conf)
             pcs.add_download_task(resource_url,
                                   '%s/%s/' % (conf.get('baidupan', 'dest_dir'),
                                               key))
@@ -99,11 +120,43 @@ def download_drama(conf):
                              key, resource_url)
                 continue
 
+            _set_history(key)
+
             logger.info('Add download task successfully. (key=%s, url=%s)',
                         key, resource_url)
 
         except Exception as e:
             logger.error('Error: %s', e.message)
+
+    if args.daemon:
+        _schedule.enter(24 * 3600, 0, download_drama, (args, ))
+
+
+def _set_history(key):
+    _history[key] = str(date.today())
+    _save_history()
+
+
+def _get_history(key):
+    value = _history.get(key)
+    if value is not None:
+        return datetime.strptime(_history.get(key), '%Y-%m-%d').date()
+
+
+def _save_history():
+    with open(_LOG_FILE, 'wb') as f:
+        pickle.dump(_history, f)
+
+
+def _load_history():
+    if not os.path.exists(_LOG_FILE):
+        return False
+
+    global _history
+    with open(_LOG_FILE, 'rb') as f:
+        _history = pickle.load(f)
+
+    return True
 
 
 def _parse_args():
@@ -111,6 +164,7 @@ def _parse_args():
 
     parser.add_argument('-d', dest='daemon', action='store_true', help='Daemon mode.')
     parser.add_argument('-c', dest='config', default=_CONF_FILE, help="Config file. Default is %s" % _CONF_FILE)
+    parser.add_argument('-l', dest='log', default=_LOG_FILE, help="Log file. Default is %s" % _LOG_FILE)
 
     return parser.parse_args()
 
@@ -120,12 +174,11 @@ _schedule = sched.scheduler(time.time, time.sleep)
 
 if __name__ == '__main__':
     args = _parse_args()
-    conf = _parse_conf(args.config)
 
     if not args.daemon:
-        download_drama(conf)
+        download_drama(args)
         sys.exit(0)
 
-    _schedule.enter(24 * 3600, 1, download_drama, (conf, ))
     logger.info('Drama downloader is running ...')
+    _schedule.enter(1, 0, download_drama, (args, ))
     _schedule.run()
---
+++ taskbar.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+# Creates a task-bar icon with balloon tip.  Run from Python.exe to see the
+# messages printed.  Right click for balloon tip.  Double click to exit.
+# original version of this demo available at http://www.itamarst.org/software/
+import pywintypes, win32api, win32con, win32gui, win32process
+import sys, os, ctypes
+
+WM_TASKBARNOTIFY = win32con.WM_USER+20
+WM_TASKBARNOTIFY_MENUITEM_SHOW = win32con.WM_USER + 21
+WM_TASKBARNOTIFY_MENUITEM_HIDE = win32con.WM_USER + 22
+WM_TASKBARNOTIFY_MENUITEM_EXIT = win32con.WM_USER + 23                 
+
+class Taskbar(object):
+    def __init__(self, cmd, tooltip):
+        self.visible = False
+        self.cmd = cmd
+
+        message_map = {
+            win32con.WM_DESTROY: self.onDestroy,
+            win32con.WM_COMMAND: self.onCommand,
+            WM_TASKBARNOTIFY : self.onTaskbarNotify,
+        }
+        # Register the Window class.
+        wc = win32gui.WNDCLASS()
+        wc.hInstance = win32api.GetModuleHandle(None)
+        wc.lpszClassName = "PythonTaskbarDemo"
+        wc.style = win32con.CS_VREDRAW | win32con.CS_HREDRAW;
+        wc.hCursor = win32gui.LoadCursor(0, win32con.IDC_ARROW)
+        wc.hbrBackground = win32con.COLOR_WINDOW
+        wc.lpfnWndProc = message_map # could also specify a wndproc.
+        classAtom = win32gui.RegisterClass(wc)
+        # Create the Window.
+        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+        self.hwnd = win32gui.CreateWindow( classAtom, "Taskbar Demo", style, \
+                    0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
+                    0, 0, wc.hInstance, None)
+        win32gui.UpdateWindow(self.hwnd)
+
+        hProcess, hThread, dwProcessId, dwThreadId = win32process.CreateProcess(None, self.cmd, None, None, 0, 0, None, None, win32process.STARTUPINFO())
+        self.hProcess = hProcess
+        try:
+            hicon, small = win32gui.ExtractIconEx(win32api.GetModuleFileName(0), 0)
+            win32gui.DestroyIcon(small[0])
+            #hicon = pywintypes.HANDLE(hicon[0])
+            hicon = hicon[0]
+        except IndexError:
+            hicon = win32gui.LoadIcon(0, win32con.IDI_APPLICATION)
+        self.hicon = hicon
+        self.tooltip = tooltip
+        self.show()
+
+    def show(self):
+        """Display the taskbar icon"""
+        flags = win32gui.NIF_ICON | win32gui.NIF_MESSAGE
+        if self.tooltip is not None:
+            flags |= win32gui.NIF_TIP
+            nid = (self.hwnd, 0, flags, WM_TASKBARNOTIFY, self.hicon, self.tooltip)
+        else:
+            nid = (self.hwnd, 0, flags, WM_TASKBARNOTIFY, self.hicon)
+        win32gui.Shell_NotifyIcon(win32gui.NIM_ADD, nid)
+        self.visible = True
+
+    def hide(self):
+        """Hide the taskbar icon"""
+        if self.visible:
+            nid = (self.hwnd, 0)
+            win32gui.Shell_NotifyIcon(win32gui.NIM_DELETE, nid)
+        self.visible = False
+
+    def onDestroy(self, hwnd, msg, wparam, lparam):
+        self.hide()
+        win32gui.PostQuitMessage(0) # Terminate the app.
+
+    def onTaskbarNotify(self, hwnd, msg, wparam, lparam):
+        if lparam == win32con.WM_LBUTTONUP:
+            self.onClick()
+        elif lparam == win32con.WM_LBUTTONDBLCLK:
+            self.onDoubleClick()
+        elif lparam ==  win32con.WM_RBUTTONUP:
+            self.onRightClick()
+        return 1
+
+    def onCommand(self, hwnd, msg, wparam, lparam):
+        nID = win32api.LOWORD(wparam)
+        hwnd = ctypes.windll.kernel32.GetConsoleWindow()
+        if nID == WM_TASKBARNOTIFY_MENUITEM_SHOW:
+            win32gui.ShowWindow(hwnd, win32con.SW_SHOW|win32con.SW_MAXIMIZE)
+        elif nID == WM_TASKBARNOTIFY_MENUITEM_HIDE:
+            win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
+        elif nID == WM_TASKBARNOTIFY_MENUITEM_EXIT:
+            win32process.TerminateProcess(self.hProcess, 0)
+            self.hide()
+            sys.exit(0)
+        return 1
+
+    def onClick(self):
+        hwnd = ctypes.windll.kernel32.GetConsoleWindow()
+        v = ctypes.windll.user32.IsWindowVisible(hwnd)
+        win32gui.ShowWindow(hwnd, {1:0,0:1}[v])
+        win32gui.SetForegroundWindow(hwnd)
+
+    def onDoubleClick(self):
+        pass
+
+    def onRightClick(self):
+        menu = win32gui.CreatePopupMenu()
+        win32gui.AppendMenu(menu, win32con.MF_STRING, WM_TASKBARNOTIFY_MENUITEM_SHOW, u'Display')
+        win32gui.AppendMenu(menu, win32con.MF_STRING, WM_TASKBARNOTIFY_MENUITEM_HIDE, u'Hide')
+        win32gui.AppendMenu(menu, win32con.MF_STRING, WM_TASKBARNOTIFY_MENUITEM_EXIT, u'Exit')
+        pos = win32gui.GetCursorPos()
+        win32gui.SetForegroundWindow(self.hwnd)
+        win32gui.TrackPopupMenu(menu, win32con.TPM_LEFTALIGN, pos[0], pos[1], 0, self.hwnd, None)
+        win32gui.PostMessage(self.hwnd, win32con.WM_NULL, 0, 0)
+
+if __name__=='__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    os.environ['PYTHONOPTIMIZE'] = 'x'
+    t = Taskbar('drama_download.exe -d ', 'Drama downloader (by goorockey)')
+    win32gui.PumpMessages()
